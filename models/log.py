4b31d6ee36d55a9a69a8452e357b1f4e28b425ef
Mark <valeckom@gmail.com>
2018-11-21 15:30:33 -0800

    default terminal printing to header and end only

--- models/log.py
+++ models/log.py
@@ -16,19 +16,22 @@ class Logger(object):
     def header(self):
         self._setup_log()
         msg = "Video Scanner v{}\r\n(c) Mark Valecko 2018".format(self.version)
-        self._write(msg)
-        self._write("{}\r\n".format(self._time_stamp()))
+        self._write(msg, ignore_verbose_lvl=True)
+        self._write("{}\r\n".format(self._time_stamp()), ignore_verbose_lvl=True)
 
     def note(self, msg):
         self._write(msg)
+
+    # def progress(self, msg):
+    #     self._write(msg)
 
     def error(self, msg):
         self._write("ERROR "+ msg)
 
     def end(self, number_of_videos_scanned):
         msg = "\r\n{} videos have been scanned\r\nfinished at {}".format(number_of_videos_scanned, self._short_time_stamp())
-        self._write(msg)
-        self._write('')
+        self._write(msg, ignore_verbose_lvl=True)
+        self._write('', ignore_verbose_lvl=True)
 
     @staticmethod
     def _time_stamp():
@@ -38,8 +41,8 @@ class Logger(object):
     def _short_time_stamp():
         return '{:%H:%M}'.format(datetime.now())
 
-    def _write(self, content):
-        self.terminal.write(content)
+    def _write(self, content, ignore_verbose_lvl=False):
+        self.terminal.write(content, ignore_verbose_lvl)
         with open(self.file, "a") as f:
             f.write('{}\r\n'.format(content))
 
